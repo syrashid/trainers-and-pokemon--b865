893940ea861796d5b6524e538cebff76d4407692
Sy Rashid <sy.rashid.11@gmail.com>
2022-05-18 17:05:33 +0100

    ARCH: Set up rails app for livecode

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # As a user I can see a trainer's pokemon
+  # As a user I can add a pokemon to a trainer (catch a pokemon)
+  # As a user I can release a pokemon (destroy)
+end
