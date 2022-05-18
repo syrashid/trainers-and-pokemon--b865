2ad689470a7d885c8b13eafa4176412893a3b2c4
Sy Rashid <sy.rashid.11@gmail.com>
2022-05-18 17:26:16 +0100

    ARCH: Add pokemon model"

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :trainers
   # As a user I can see a trainer's pokemon
   # As a user I can add a pokemon to a trainer (catch a pokemon)
   # As a user I can release a pokemon (destroy)
